a760934c79570952b36964aa2969c479680f105f
Alexandre <pradoromeu@hotmail.com>
2023-11-13 14:33:13 -0300

    Arquivo de configuração

--- Connection/config.php
+++ Connection/config.php
@@ -1,8 +1,8 @@
 <?php
-define("EMAIL_ORIGEM","contato@aprcompany.eng.br");     // E-mail de origem, utilizado para disparo                    
-define("SENHA_EMAIL","Mudar@102030");                   // Senha do e-mail de origem
-define("EMAIL_DESTINO","contato@aprcompany.eng.br");    // Caixa postal que vai receber os e-mails de contato do formulário
-define("PORTA",465);                                    // Porta 465 (SSL) / 587(TLS)
-define("CRIPTOGRAFIA","ssl");                           // Criptografia utilizada na conexão - SSL/TLS
-define("SERVIDOR","email-ssl.com.br");                  // Servidor de e-mail SMTP  
+define("EMAIL_ORIGEM","email origem");                        // E-mail de origem, utilizado para disparo                    
+define("SENHA_EMAIL","senha email origem");                   // Senha do e-mail de origem
+define("EMAIL_DESTINO","email destinatario");                 // Caixa postal que vai receber os e-mails de contato do formulário
+define("PORTA",465);                                          // Porta 465 (SSL) / 587(TLS)
+define("CRIPTOGRAFIA","ssl");                                 // Criptografia utilizada na conexão - SSL/TLS
+define("SERVIDOR","servidor de e-mail");                      // Servidor de e-mail SMTP  
 ?>
